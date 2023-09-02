8d1401b0ac33ac7841d09b7be20c54e2bca62a66
kaveesha <savindukaveesha11@gmail.com>
2023-09-02 10:28:27 +0530

    sdsd

--- src/pages/customer/Home.js
+++ src/pages/customer/Home.js
@@ -3,6 +3,13 @@ import './Home.css'
 export const Home = () => {
     return (<div>
         <div className='Hometop'>
+            <div className='Hometoptopic'>
+                <h1 className='Hometoph1'>GEAR.COM</h1>
+                <h3 className='Hometoph3'> Fuel your ambition with fitness</h3>
+            </div>
+
+            <button className='Hometopbutton'>Shop Now</button>
+
 
         </div>
     </div>);
