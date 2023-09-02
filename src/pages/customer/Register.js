935589990c9bda1f8f3069dfb55249e73a559269
kaveesha <savindukaveesha11@gmail.com>
2023-09-02 14:19:24 +0530

    qwqw

--- src/pages/customer/Register.js
+++ src/pages/customer/Register.js
@@ -1,3 +1,58 @@
+import { useState } from 'react';
+import './Register.css'
+import axios from 'axios';
+
 export const Register = () => {
-    return <div>Register</div>
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [contactno, setContact] = useState("");
+    const [dateofbirth, setDOB] = useState("");
+    const [password, setPassword] = useState("");
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const hello = await axios.post('http://localhost:3001/Customer/register', { username, email, dateofbirth, contactno, password });
+            alert("Registration Completed! Now login");
+
+        }
+        catch (error) {
+            alert("Email Have Already Taken");
+
+        }
+    }
+
+    return (
+        <div className="registermain">
+            <form onSubmit={onSubmit} className='registerform' action="">
+
+                <h2 className='registerformh1'>Join Now For Free</h2>
+
+                <div className='registerformgroup'>
+                    <label className="registerformlabel" htmlFor="username">Name</label>
+                    <input className="registerforminput" required minLength={5} value={username} type="text" id="username" onChange={(event) => { setUsername(event.target.value) }} />
+                </div>
+                <div className='registerformgroup'>
+                    <label className="registerformlabel" htmlFor="email">Email</label>
+                    <input className="registerforminput" required minLength={5} value={email} type="email" id="email" onChange={(event) => { setEmail(event.target.value) }} />
+                </div>
+                <div className='registerformgroup'>
+                    <label className="registerformlabel" htmlFor="contactnumber">Contact Number</label>
+                    <input className="registerforminput" required minLength={5} value={contactno} type="number" id="contactnumber" onChange={(event) => { setContact(event.target.value) }} />
+                </div>
+                <div className='registerformgroup'>
+                    <label className="registerformlabel" htmlFor="dob">Date of Birth</label>
+                    <input className="registerforminput" required minLength={5} value={dateofbirth} type="text" id="dob" onChange={(event) => setDOB(event.target.value)} />
+                </div>
+                <div className='registerformgroup'>
+                    <label className="registerformlabel" htmlFor="password">Password</label>
+                    <input className="registerforminput" required minLength={5} value={password} type="password" id="password" onChange={(event) => { setPassword(event.target.value) }} />
+                </div>
+                <div className='regbutton '>
+                    <button type='submit' className='registerbutton'>Register</button>
+                </div>
+
+            </form >
+        </div >
+    );
 }
